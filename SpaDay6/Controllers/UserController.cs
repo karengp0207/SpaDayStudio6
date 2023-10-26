0e0f4fe7833f93f9c0d5ae0dc93687c87af81994
karengp0207 <karengp0207@gmail.com>
2023-10-25 19:37:06 -0500

    ready for user validation

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay6.Models;
+
+
+namespace SpaDay6.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match!  Try again!";
+                ViewBag.userName = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Add");
+            }
+        }
+    }
+}
+    
+
